32c0feb5c3e622fe25f01ebb642e2551d173dd95
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2018-12-28 00:11:59 +0100

    Changed response

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -3,6 +3,6 @@ import { Response, Request } from 'express';
 export default class appController {
 
     public uploadPackages = (req: Request, res: Response) => {
-        res.status(200).json('upload packages');
+        res.status(200).json(req.files);
     }
 }
