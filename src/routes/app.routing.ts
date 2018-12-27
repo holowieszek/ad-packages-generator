d516f5101b2f7271e3de2ec69fa06410f541493c
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2018-12-28 00:12:25 +0100

    Added upload middleware

--- src/routes/app.routing.ts
+++ src/routes/app.routing.ts
@@ -1,9 +1,11 @@
 import appController from '../controllers/app.controller';
+import Upload from '../middlewares/upload.middleware';
 
 export class appRouting {
     private appController: appController = new appController();
-    
+    private upload: Upload = new Upload();
+
     routes(app): void {
-        app.route('/').get(this.appController.uploadPackages);
+        app.route('/').post(this.upload.extractFile(), this.appController.uploadPackages);
     }
 }
