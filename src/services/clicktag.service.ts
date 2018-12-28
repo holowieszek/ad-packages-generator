123aaf27482af715e189690cbcbc7ecc7cb0cec8
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2018-12-28 22:55:16 +0100

    Clicktags Service - get all rows & get by name

--- src/services/clicktag.service.ts
+++ src/services/clicktag.service.ts
@@ -2,7 +2,16 @@ import Clicktag from '../models/clicktag.model';
 import { clicktagInterface as IClicktag } from '../interfaces/clicktag.interface';
 
 export class clicktagService {
+
+    getAllClicktags() {
+        return Clicktag.find({});
+    }
+
     createClicktag(data) {
         return Clicktag.create(data);
+    }
+
+    getClicktagByName(name: string) {
+        return Clicktag.find({ name: name });
     }
 }
