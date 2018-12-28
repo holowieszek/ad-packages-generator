7d891147b5eb02362b297bb97b9378d39ab9e749
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2018-12-28 22:54:49 +0100

    Clicktag routing GET & POST & GET by name

--- src/routes/clicktag.routing.ts
+++ src/routes/clicktag.routing.ts
@@ -4,6 +4,11 @@ export class clicktagRouting {
     private clicktagController: clicktagController = new clicktagController();
 
     routes(app): void {
-        app.route('/clicktags').post(this.clicktagController.createClicktag);
+        app.route('/clicktags')
+            .get(this.clicktagController.getAllClicktags)
+            .post(this.clicktagController.createClicktag);
+
+        app.route('/clicktags/:name')
+            .get(this.clicktagController.getClicktagByName)
     }
 }
